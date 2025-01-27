99a8860d8d023f43fcd664467578ed40d726552c
manish23 <manishdoley23@gmail.com>
2025-01-27 17:44:48 +0530

    patch: tw classname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Search, Users, Database } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 sm:p-8 pb-20 gap-8 sm:gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-8 sm:gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-center w-full">
         <div className="text-center space-y-4 px-2">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
