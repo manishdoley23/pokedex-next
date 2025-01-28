760a1a33a2d6e0b3a5342b677dde5329419bb87d
manish23 <manishdoley23@gmail.com>
2025-01-28 07:56:03 +0530

    patch: remove isr

--- src/app/pokedex/[pokemon-id]/page.tsx
+++ src/app/pokedex/[pokemon-id]/page.tsx
@@ -1,52 +1,26 @@
 import { Suspense } from "react";
 import PokemonContainer from "@/components/pokemon/pokemon-container";
 import { PokemonContainerLoadingSkeleton } from "@/components/pokemon/pokemon-container-loading-skeleton";
-import { _fetch, BASE_URL } from "@/lib/api/config";
 import { getPokemonById } from "@/lib/api/pokemon";
 import { notFound } from "next/navigation";
-
-// Revalidate cache every 60 seconds
-export const revalidate = 60;
-
-// Allow dynamic paths
-export const dynamicParams = true;
-
-// Generate static params at build time
-export async function generateStaticParams() {
-  try {
-    const data = await _fetch(`${BASE_URL}/pokemon`);
-
-    return data.results.map((pokemon: { url: string }) => ({
-      "pokemon-id": pokemon.url.split("/").slice(-2)[0],
-    }));
-  } catch (error) {
-    console.error("Error generating static params:", error);
-    return [];
-  }
-}
 
 export default async function PokemonPage({
   params,
 }: {
   params: Promise<{ "pokemon-id": string }>;
 }) {
-  try {
-    const pokemonId = await params;
-    const pokemon = await getPokemonById(parseInt(pokemonId["pokemon-id"]));
+  const pokemonId = await params;
+  const pokemon = await getPokemonById(parseInt(pokemonId["pokemon-id"]));
 
-    if (!pokemon) {
-      notFound();
-    }
-
-    return (
-      <main className="container mx-auto p-4">
-        <Suspense fallback={<PokemonContainerLoadingSkeleton />}>
-          <PokemonContainer pokemon={pokemon} />
-        </Suspense>
-      </main>
-    );
-  } catch (error) {
-    console.error("Error fetching Pokemon:", error);
+  if (!pokemon) {
     notFound();
   }
+
+  return (
+    <main className="container mx-auto p-4">
+      <Suspense fallback={<PokemonContainerLoadingSkeleton />}>
+        <PokemonContainer pokemon={pokemon} />
+      </Suspense>
+    </main>
+  );
 }
